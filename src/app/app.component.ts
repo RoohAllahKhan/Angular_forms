70d8c6318a3d0721006ec3017f1920fef0026fa2
RoohAllahKhan <roohallahkhan20@gmail.com>
2021-11-16 18:35:03 +0500

    Set value to set the whole form object and Patch value to set any particular key of form object.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,21 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // NOT THE BEST APPROACH
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
